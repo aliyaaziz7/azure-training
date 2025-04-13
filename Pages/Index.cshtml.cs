c05f79efbdb65550d9a79aff7603526f8a000058
Aliya Aziz <aliya.aziz@wedo-digital.co.uk>
2025-04-13 16:45:47 +0100

    appsetting message

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,14 +6,16 @@ namespace VisualStudio.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _config;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _config = configuration;
     }
 
     public void OnGet()
     {
-
+        ViewData["Message"] = _config["Greeting"] ?? "Hello, World!";
     }
 }
